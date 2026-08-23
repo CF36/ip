c43119407ffbbfc72d4b3fec4ce77f11d4230938
Chang Feng <changfeng.cf.lee@gmail.com>
2026-08-23 16:00:07 +0800

    Renamed chatbot to Slowbro

---
+++ src/main/java/SlowBro.java
@@ -0,0 +1,10 @@
+public class SlowBro {
+    public static void main(String[] args) {
+        String banner = "  ____  _               _                \n"
+                + " / ___|| | ___  _     | |__  _ __ ___  \n"
+                + " \\___ \\| |/ _ \\ \\ /\\ / / '_ \\| '__/ _ \\\n"
+                + "  ___) | | (_) \\ V  V /| |_) | | | (_) |\n"
+                + " |____/|_|\\___/ \\_/\\_/ |_.__/|_|  \\___/\n";
+        System.out.println(banner);
+    }
+}
