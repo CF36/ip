6cca8c00ed15ded4412a4160ab0a2b53ab7782d2
Chang Feng <changfeng.cf.lee@gmail.com>
2026-08-23 16:03:52 +0800

    Added simple greeting

--- src/main/java/SlowBro.java
+++ src/main/java/SlowBro.java
@@ -1,10 +1,22 @@
 public class SlowBro {
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
+
         String banner = "  ____  _               _                \n"
-                + " / ___|| | ___  _     | |__  _ __ ___  \n"
+                + " / ___|| | ___         | |__  _ __ ___  \n"
                 + " \\___ \\| |/ _ \\ \\ /\\ / / '_ \\| '__/ _ \\\n"
                 + "  ___) | | (_) \\ V  V /| |_) | | | (_) |\n"
                 + " |____/|_|\\___/ \\_/\\_/ |_.__/|_|  \\___/\n";
-        System.out.println(banner);
+
+        // Greeting
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Slowbro.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+
+        // Exit
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
